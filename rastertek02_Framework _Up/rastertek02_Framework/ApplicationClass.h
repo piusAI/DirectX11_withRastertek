13da22b5fe38e39aa643c2b0bdd21d04a234c0c4
PIUS <hwangpiusjoon@gmail.com>
2026-04-15 15:12:30 +0900

    Add files via upload

---
+++ rastertek02_Framework _Up/rastertek02_Framework/ApplicationClass.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include <Windows.h>
+
+// 시스템 클래스에서 생성되는 또 다른 객체임
+// 모든 app 그래픽 기능 여기 클래스에 캡슐화!
+// 향후 모든 그래픽 객체 포함할거임!
+
+//////////////
+// 전역 변수 //
+// ///////////
+const bool FULL_SCREEN = false;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 100.f;
+const float SCREEN_NEAR = 0.3f;
+
+class ApplicationClass
+{
+	public:
+		ApplicationClass() {}
+		ApplicationClass(const ApplicationClass&) {}
+		~ApplicationClass() {}
+
+		bool Initialize(int, int, HWND);
+		void Shutdown();
+		bool Frame();
+
+private:
+		bool Render();
+};
+
